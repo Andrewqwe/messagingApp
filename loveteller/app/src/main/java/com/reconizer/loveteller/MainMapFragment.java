65cc9eedf661542ce4dfb5f1b0e4d7bda5c59df9
Andrzej <andrew1q2w@gmail.com>
2017-08-11 00:27:00 +0200

    skytower 11.08

--- loveteller/app/src/main/java/com/reconizer/loveteller/MainMapFragment.java
+++ loveteller/app/src/main/java/com/reconizer/loveteller/MainMapFragment.java
@@ -1,28 +1,104 @@
 package com.reconizer.loveteller;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
 /**
  * Created by Andrzej on 2017-08-06.
  */
 
-public class MainMapFragment extends Fragment {
+public class MainMapFragment extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap googleMap;
+    MapView mapView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View myFragmentView = inflater.inflate(R.layout.main_map_fragment, container, false);
+
+
+        mapView = (MapView) myFragmentView.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+        //map.getUiSettings().setMyLocationButtonEnabled(false);
+
         return myFragmentView;
     }
 
-    public static MainMapFragment newInstance(){
+    public static MainMapFragment newInstance() {
         MainMapFragment f = new MainMapFragment();
 // https://stackoverflow.com/questions/18413309/how-to-implement-a-viewpager-with-different-fragments-layouts
 // tutorial z ktorego korzystalem
         return f;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        googleMap = map;
+        startMap();
+
+    }
+
+    public void startMap() {
+        if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            String x[] = new String[2];
+            ActivityCompat.requestPermissions(this.getActivity(), x, 1);
+
+            return;
+        };
+        googleMap.setIndoorEnabled(true);
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+
+        LatLng sydney = new LatLng(-33.867, 151.206);
+        googleMap.setMyLocationEnabled(true);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15));
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }

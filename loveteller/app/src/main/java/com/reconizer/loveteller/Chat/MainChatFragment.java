3b76e06bebc3983e422f7de17a74dee004d8316c
Damian Łondka <dlondka@op.pl>
2017-08-15 18:03:27 +0200

    podstawy chatu

--- loveteller/app/src/main/java/com/reconizer/loveteller/Chat/MainChatFragment.java
+++ loveteller/app/src/main/java/com/reconizer/loveteller/Chat/MainChatFragment.java
@@ -1,4 +1,4 @@
-package com.reconizer.loveteller;
+package com.reconizer.loveteller.Chat;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -6,16 +6,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.reconizer.loveteller.R;
 
 /**
  * Created by Andrzej on 2017-08-06.
  */
 
 public class MainChatFragment extends Fragment {
+    private ListView list_view;
+    private ArrayAdapter<Conversation> adapter;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View myFragmentView = inflater.inflate(R.layout.main_chat_fragment, container, false);
+
         return myFragmentView;
     }
     public static MainChatFragment newInstance(){

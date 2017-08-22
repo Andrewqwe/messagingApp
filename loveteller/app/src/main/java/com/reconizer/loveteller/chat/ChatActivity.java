efa27d8d87f49ac5c371a3254c8d100ab9299f09
Andrzej <andrew1q2w@gmail.com>
2017-08-22 23:54:12 +0200

    skalowanie dymkow czatu i kolorki

--- loveteller/app/src/main/java/com/reconizer/loveteller/chat/ChatActivity.java
+++ loveteller/app/src/main/java/com/reconizer/loveteller/chat/ChatActivity.java
@@ -41,7 +41,7 @@ public class ChatActivity extends AppCompatActivity {
             Message message = new Message(0,"Czesc");
             messageList.add(message);
 
-            message = new Message(0,"No hej");
+            message = new Message(0,"No hej :)");
             messageList.add(message);
 
             message = new Message(0,"Co slychac ?");
@@ -50,12 +50,15 @@ public class ChatActivity extends AppCompatActivity {
             message = new Message(0,"Wszystko w porzadku a u ciebie ?");
             messageList.add(message);
 
-            message = new Message(0,"Nie wiem jak zrobic dynamiczne skalowanie tych pieprzonych dymkow.");
+            message = new Message(0,"Nareszcie działa to dynamiczne skalowanie dymków. Przydał by się jakiś guzik do wysyłania tej wiadomosci" +
+                    " i powoli mozna zaczac uzywac tego czatu");
             messageList.add(message);
 
-            message = new Message(0,"TO STRASZNE !");
+            message = new Message(0,"Widze ze dobrze ci idzie, ciekawe jak radzi sobie reszta");
             messageList.add(message);
 
+            message = new Message(0,"To fajna ekipa, na pewno mnie nie zawiodą ! :)");
+            messageList.add(message);
 
             chatAdapter.notifyDataSetChanged();
         }
